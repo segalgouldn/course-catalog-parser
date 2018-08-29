34bfe00e9fdddd35e70c95d6b679482c51374654
Noah Segal-Gould <ns2349@bard.edu>
2018-08-29 17:54:30 -0400

    Updated the parser and server to work with recent course lists. Also updated the account registration page and fixed a typo on the locations list.

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # Course Catalog Parser
 Using Beautiful Soup to scrape the courses from Bard College course catalogs into JSON files.
 
-The project will have three parts:
-* Web-scraping course catalogs ✔️
-* Better serving course catalogs (compared to the current site) **¯\\(ツ)/¯**
-* Machine learning to predict future potential course catalogs
-
 At the moment, the parser program `app.py` takes around 30 seconds to parse through a semester's worth of courses. Across semesters, the layout of Bard's course catalog is very inconsistent. This has made it necessary to distinguish course catalogs which separate dates and times within tables from those which do not, as well as those course catalogs which use the two-character distributions naming system (which evidently started in Fall 2016) from those which use the four-character system.
 
 This project was partially inspired by [@sabo](https://github.com/sabo).
 
 P.S. Sorry for my ridiculous list comprehensions.
 
+### TODO
+* There's currently an issue with inconsistencies in the layout of tables within **individual** course list pages.
+  * For example, see the [mathematics department course list for Spring 2018](http://inside.bard.edu/academic/courses/spring2018/mathematics.html).
+  * Because the parser is not designed to automatically distinguish between course list pages which do and do not feature both the new and old distributions naming systems, pages such as this one can not be parsed.
+  * To fix this, it should be possible to programmatically count the number of columns in each table for each course in each course list page, thus informing the parser on which courses use both or neither of the distributions naming systems.
+
 ### Related Repositories:
 * [Course Catalog Server](https://github.com/segalgouldn/course-catalog-server)
 * [Course Catalog Classifier](https://github.com/segalgouldn/course-catalog-classifier)
+
+This was intended as a [Bard College Senior Project](https://github.com/segalgouldn/Senior-Project-Subtweets/blob/master/drafts/senior_project_guidelines.pdf), but I decided to make [this](https://github.com/segalgouldn/Senior-Project-Subtweets) instead.
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Noah Segal-Gould, Summer 2017
+# Noah Segal-Gould, 28 August 2018
 
 import requests
 import json
@@ -18,19 +18,25 @@ def make_course_list(urls, has_new_distributions=True, division_between_dates_an
         unchunked_widths = [int(w.get("width", "0")) for w in soup.find_all("td")]
         widths = [x for x in [unchunked_widths[i:i + 7] for i in range(0, len(unchunked_widths), 7)] if 0 not in x]
 
-        course_registration_numbers = []
+        course_reg_numbers = []
         for i in range(len(widths)):
-            course_registration_numbers = [int(crn.text.replace("\n", " ").split()[0]) for crn in soup.find_all("td", {"width": str(widths[i][0])}) if len(crn.text) >= 5]
-            if len(course_registration_numbers) >= 1:
+            course_reg_numbers = [int(crn.text.replace("\n", " ").split()[0]) for crn in
+                                  soup.find_all("td", {"width": str(widths[i][0])}) if len(crn.text) >= 5]
+            if len(course_reg_numbers) >= 1:
                 break
 
         course_codes = []
         course_titles = []
         for i in range(len(widths)):
             try:
-                course_numbers_and_titles = [[p.text for p in td.find_all("p")] for td in soup.find_all("td", {"width": str(widths[i][1])})]
-                course_codes = [" ".join(d[0].replace("\xa0", "").replace("\r\n", "").replace("\n", "").replace("\r", "").strip().split()) for d in course_numbers_and_titles]
-                course_titles = [" ".join(d[1].replace("\xa0", "").replace("\r\n", "").replace("\n", "").replace("\r", "").strip().split()) for d in course_numbers_and_titles]
+                course_numbers_and_titles = [[p.text for p in td.find_all("p")] for td in
+                                             soup.find_all("td", {"width": str(widths[i][1])})]
+                course_codes = [" ".join(
+                    d[0].replace("\xa0", "").replace("\r\n", "").replace("\n", "").replace("\r", "").strip().split())
+                                for d in course_numbers_and_titles]
+                course_titles = [" ".join(
+                    d[1].replace("\xa0", "").replace("\r\n", "").replace("\n", "").replace("\r", "").strip().split())
+                                 for d in course_numbers_and_titles]
                 if len(course_codes) >= 1 and len(course_list) >= 1:
                     break
             except IndexError:
@@ -38,21 +44,34 @@ def make_course_list(urls, has_new_distributions=True, division_between_dates_an
 
         professors = []
         for i in range(len(widths)):
-            professors = [" ".join(prof.text.replace("\xa0", " ").replace("\n", " ").replace("\r", " ").replace("Lab:", "").replace("LAB:", "").replace("Screening:", "").strip().split()) for prof in soup.find_all("td", {"width": str(widths[i][2])})]
+            professors = [" ".join(
+                prof.text.replace("\xa0", " ").replace("\n", " ").replace("\r", " ").replace("Lab:", "").replace("LAB:",
+                                                                                                                 "").replace(
+                    "Screening:", "").strip().split()) for prof in soup.find_all("td", {"width": str(widths[i][2])})]
             if len(professors) >= 1:
                 break
 
         schedules = []
         if has_new_distributions and not division_between_dates_and_times:
             for i in range(len(widths)):
-                schedules = [" ".join(dt.text.replace("\xa0", " ").replace("\n", " ").replace("\r", " ").replace(" \u2013 ", "-").replace(".", " ").strip().split()) for dt in soup.find_all("td", {"width": str(widths[i][3])})]
+                schedules = [" ".join(
+                    dt.text.replace("\xa0", " ").replace("\n", " ").replace("\r", " ").replace(" \u2013 ", "-").replace(
+                        ".", " ").strip().split()) for dt in soup.find_all("td", {"width": str(widths[i][3])})]
                 if len(schedules) >= 1:
                     break
         else:
             for i in range(len(widths)):
                 try:
-                    schedules_part_1 = [" ".join(dt.text.replace("\xa0", " ").replace("\n", " ").replace("\r", " ").replace(" \u2013 ", "-").replace(".", " ").strip().split()) for dt in soup.find_all("td", {"width": str(widths[i][3])})]
-                    schedules_part_2 = [" ".join(dt.text.replace("\xa0", " ").replace("\n", " ").replace("\r", " ").replace(" \u2013 ", "-").replace(".", " ").strip().split()) for dt in soup.find_all("td", {"width": str(widths[i][4])})]
+                    schedules_part_1 = [" ".join(
+                        dt.text.replace("\xa0", " ").replace("\n", " ").replace("\r", " ").replace(" \u2013 ",
+                                                                                                   "-").replace(".",
+                                                                                                                " ").strip().split())
+                                        for dt in soup.find_all("td", {"width": str(widths[i][3])})]
+                    schedules_part_2 = [" ".join(
+                        dt.text.replace("\xa0", " ").replace("\n", " ").replace("\r", " ").replace(" \u2013 ",
+                                                                                                   "-").replace(".",
+                                                                                                                " ").strip().split())
+                                        for dt in soup.find_all("td", {"width": str(widths[i][4])})]
                     schedules = [schedules_part_1[j] + " " + schedules_part_2[j] for j in range(len(schedules_part_2))]
                     if len(schedules) >= 1:
                         break
@@ -62,17 +81,20 @@ def make_course_list(urls, has_new_distributions=True, division_between_dates_an
         locations = []
         if has_new_distributions and not division_between_dates_and_times:
             for i in range(len(widths)):
-                locations = [dt.text.replace("\xa0", " ").replace("\n", " ").replace("\r", "").strip() for dt in soup.find_all("td", {"width": str(widths[i][4])})]
+                locations = [dt.text.replace("\xa0", " ").replace("\n", " ").replace("\r", "").strip() for dt in
+                             soup.find_all("td", {"width": str(widths[i][4])})]
                 if len(locations) >= 1:
                     break
         elif not has_new_distributions and not division_between_dates_and_times:
             for i in range(len(widths)):
-                locations = [dt.text.replace("\xa0", " ").replace("\n", " ").replace("\r", "").strip() for dt in soup.find_all("td", {"width": str(widths[i][4])})]
+                locations = [dt.text.replace("\xa0", " ").replace("\n", " ").replace("\r", "").strip() for dt in
+                             soup.find_all("td", {"width": str(widths[i][4])})]
                 if len(locations) >= 1:
                     break
         else:
             for i in range(len(widths)):
-                locations = [dt.text.replace("\xa0", " ").replace("\n", " ").replace("\r", "").strip() for dt in soup.find_all("td", {"width": str(widths[i][5])})]
+                locations = [dt.text.replace("\xa0", " ").replace("\n", " ").replace("\r", "").strip() for dt in
+                             soup.find_all("td", {"width": str(widths[i][5])})]
                 if len(locations) >= 1:
                     break
 
@@ -80,7 +102,8 @@ def make_course_list(urls, has_new_distributions=True, division_between_dates_an
         if has_new_distributions and not division_between_dates_and_times:
             for i in range(len(widths)):
                 try:
-                    old_distributions = [dt.text.replace("\xa0", "").replace("\n", " ").replace("\r", " ").strip() for dt in soup.find_all("td", {"width": str(widths[i][6])})]
+                    old_distributions = [dt.text.replace("\xa0", "").replace("\n", " ").replace("\r", " ").strip() for
+                                         dt in soup.find_all("td", {"width": str(widths[i][6])})]
                     if len(old_distributions) >= 1:
                         break
                 except IndexError:
@@ -88,7 +111,8 @@ def make_course_list(urls, has_new_distributions=True, division_between_dates_an
         elif not has_new_distributions and division_between_dates_and_times:
             for i in range(len(widths)):
                 try:
-                    old_distributions = [dt.text.replace("\xa0", "").replace("\n", " ").replace("\r", " ").strip() for dt in soup.find_all("td", {"width": str(widths[i][6])})]
+                    old_distributions = [dt.text.replace("\xa0", "").replace("\n", " ").replace("\r", " ").strip() for
+                                         dt in soup.find_all("td", {"width": str(widths[i][6])})]
                     if len(old_distributions) >= 1:
                         break
                 except IndexError:
@@ -96,28 +120,33 @@ def make_course_list(urls, has_new_distributions=True, division_between_dates_an
         elif not has_new_distributions and not division_between_dates_and_times:
             for i in range(len(widths)):
                 try:
-                    old_distributions = [dt.text.replace("\xa0", "").replace("\n", " ").replace("\r", " ").strip() for dt in soup.find_all("td", {"width": str(widths[i][5])})]
+                    old_distributions = [dt.text.replace("\xa0", "").replace("\n", " ").replace("\r", " ").strip() for
+                                         dt in soup.find_all("td", {"width": str(widths[i][5])})]
                     if len(old_distributions) >= 1:
                         break
                 except IndexError:
                     break
         else:
-            old_distributions = [""] * len(course_registration_numbers)
+            old_distributions = [""] * len(course_reg_numbers)
 
         new_distributions = []
         if has_new_distributions and not division_between_dates_and_times:
             for i in range(len(widths)):
-                new_distributions = [dt.text.replace("\xa0", "").replace("\n", " ").replace("\r", " ").strip() for dt in soup.find_all("td", {"width": str(widths[i][5])})]
+                new_distributions = [dt.text.replace("\xa0", "").replace("\n", " ").replace("\r", " ").strip() for dt in
+                                     soup.find_all("td", {"width": str(widths[i][5])})]
                 if len(new_distributions) >= 1:
                     break
         else:
-            new_distributions = [""] * len(course_registration_numbers)
+            new_distributions = [""] * len(course_reg_numbers)
 
-        descriptions = [s.parent.find_next_sibling('p').text.replace(b"\xd0\xb0".decode("utf-8"), "").replace("\xa0", "").replace("\r\n", " ").replace("\u201c", "\"").replace("\u201d", "\"") for s in soup.find_all("tr")]
+        descriptions = [
+            s.parent.find_next_sibling('p').text.replace(b"\xd0\xb0".decode("utf-8"), "").replace("\xa0", "").replace(
+                "\r\n", " ").replace("\u201c", "\"").replace("\u201d", "\"") for s in soup.find_all("tr") if
+            s.parent.find_next_sibling('p') is not None]
 
-        urls = [url] * len(course_registration_numbers)
-        seasons = [season] * len(course_registration_numbers)
-        departments = [department] * len(course_registration_numbers)
+        urls = [url] * len(course_reg_numbers)
+        seasons = [season] * len(course_reg_numbers)
+        departments = [department] * len(course_reg_numbers)
 
         final_list = [{"course_registration_number": crn,
                        "course_code": c,
@@ -130,7 +159,7 @@ def make_course_list(urls, has_new_distributions=True, division_between_dates_an
                        "season": se,
                        "department": d,
                        "description": de,
-                       "course_title": ct} for crn, c, p, s, l, n, o, u, se, d, de, ct in zip(course_registration_numbers,
+                       "course_title": ct} for crn, c, p, s, l, n, o, u, se, d, de, ct in zip(course_reg_numbers,
                                                                                               course_codes,
                                                                                               professors,
                                                                                               schedules,
@@ -156,8 +185,22 @@ def process_input(filename, has_new_distributions=True, division_between_dates_a
             json.dump(final_output, fn, sort_keys=True, indent=4)
             end = timer()
             print("Time elapsed: " + str(round(end - start)) + " seconds")
+        return final_output
 
 
-process_input("urls_new_distributions.txt")
-process_input("urls_spring_2016.txt", has_new_distributions=False, division_between_dates_and_times=False)
-process_input("urls_old_distributions.txt", has_new_distributions=False, division_between_dates_and_times=True)
+def main():
+    top_courses = process_input("urls_new_distributions.txt")
+    middle_courses = process_input("urls_spring_2016.txt",
+                                   has_new_distributions=False,
+                                   division_between_dates_and_times=False)
+    bottom_courses = process_input("urls_old_distributions.txt",
+                                   has_new_distributions=False,
+                                   division_between_dates_and_times=True)
+
+    all_courses = top_courses + middle_courses + bottom_courses
+    with open("bard_courses.json", "w") as fn:
+        json.dump(all_courses, fn, sort_keys=True, indent=4)
+    print("Total output number of courses: " + str(len(all_courses)))
+
+
+main()
